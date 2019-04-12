16f369fb0f80cafa7968210bb550b343d01801e2
lzk <995914197@qq.com>
2019-04-13 01:02:29 +0800

    lzk 首页知识的、主题圈

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -53,6 +53,60 @@
                     <card-artical></card-artical>
                     <card-artical></card-artical>
                 </ul>
+
+                <!-- 知识点 -->
+                <h1 style="margin:50px 0 20px">知识点</h1>
+                <ul class="knowledge-wrap">
+                    <li v-for="(item,index) in 4" :key="index">
+                        <h5>8分钟学会LOGO的设计语言？</h5>
+                        <!-- <video src=""></video> -->
+                        <img class="know-video" src="@/assets/images/temp/video-banner.png" >
+                        <div class="know-msg">
+                            <div class="know-author">
+                                <img src="@/assets/images/temp/header.png" >
+                                <p>你丫才美工</p>
+                                <span>《 设计大师是如何成长的》</span>
+                            </div>
+                            <div class="know-hot">
+                                <img src="@/assets/images/temp/browse-big.png" >
+                                <span>18274</span>
+                                <img src="@/assets/images/temp/collect.png" >
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+
+                <!-- 主题圈 -->
+                <h1 style="margin:50px 0 20px">主题圈</h1>
+                <ul class="theme-wrap">
+                    <li v-for="(item,index) in 8" :key="index">
+                        <div class="theme-img">
+                            <img src="@/assets/images/temp/theme-banner.png" >
+                            <img src="@/assets/images/temp/homework-list.png" >
+                        </div>
+                        <div class="theme-title">
+                            <h5>UI设计的奇妙世界</h5>
+                            <p>微笑的冰欺凌，大厂丰富经验</p>
+                            <div><span>试看</span>为什么你的界面总是不耐看？</div>
+                        </div>
+                        <div class="theme-msg">
+                            <div>
+                                <p>主题圈介绍</p>
+                                <div>
+                                    <span>已更新23章</span>
+                                    <span>2101人参加</span>
+                                </div>
+                            </div>
+                            <p>在奇妙世界每天会有实际项目中遇见的各种问题解析，带领大家提高设计的认知，指引设计师找到方向且提高设计能力，在学习过程中，优化设计思维，结交志同道合的设计...</p>
+                            <dl>
+                                <dt>￥398/年</dt>
+                                <dd>
+                                    <img src="@/assets/images/temp/join-button.png" >
+                                </dd>
+                            </dl>
+                        </div>
+                    </li>
+                </ul>
             </div>
             <!-- 右边 -->
             <div class="warp-right">
@@ -349,6 +403,220 @@ export default {
         }
         
     }
+    // 知识点
+    .knowledge-wrap{
+       >li{
+           border-bottom:1px solid rgba(242,242,242,1);
+           >h5{
+               font-size:18px;
+                font-family:PingFangHK-Medium;
+                font-weight:500;
+                color:rgba(51,51,51,1);
+                line-height:18px;
+                padding: 15px 0;
+           }
+           .know-video{
+               display: block;
+               height: 442px;
+               width: 100%;
+           }
+           .know-msg{
+               display: flex;
+               justify-content: space-between;
+               align-items: center;
+               height: 54px;
+               .know-author{
+                   display: flex;
+                   align-items: center;
+                   >img{
+                       width: 24rpx;
+                       height: 24rpx;
+                       border-radius: 50%;
+                       margin-right: 10px;
+                   }
+                   >span{
+                       position: relative;
+                       padding-left: 15px;
+                       margin-left: 15px;
+                       &::after{
+                           position: absolute;
+                           content: '';
+                           left: 0;
+                           top: 50%;
+                           transform: translateY(-50%);
+                           height: 7px;
+                           width: 1px;
+                           background: rgba(151,151,151,1);
+                       }
+                   }
+                   >p,>span{
+                       font-size:14px;
+                        font-family:PingFangHK-Medium;
+                        font-weight:500;
+                        color:rgba(102,102,102,1);
+                        line-height:24px;
+                   }
+               }
+               .know-hot{
+                   display: flex;
+                   align-items: center;
+                   >img{
+                       display: block;
+
+                   }
+                   >span{
+                       font-size:14px;
+                        font-family:PingFangSC-Regular;
+                        font-weight:400;
+                        color:rgba(180,180,180,1);
+                        line-height:24px;
+                        margin: 0 30px 0 10px;
+                   }
+               }
+           }
+           
+       } 
+    }
+    //主题圈
+    .theme-wrap{
+        display: flex;
+        flex-wrap: wrap;
+        >li{
+            box-sizing: border-box;
+            margin-bottom: 20px;
+            overflow: hidden;
+            width:365px;
+            height:416px;
+            border-radius:8px;
+            border:1px solid rgba(84,120,235,0.1);
+            margin-right: 20px;
+            &:nth-child(2n+2){
+                margin-right: 0;
+            }
+            .theme-img{
+                >img{
+                    &:nth-child(1){
+                        width: 100%;
+                        display: block;
+                        height: 88px;
+                    }
+                    &:nth-child(2){
+                        width: 46px;
+                        height: 46px;
+                        border-radius: 50%;
+                        overflow: hidden;
+                        display: block;
+                        margin-top: -23px;
+                        margin-left: 20px;
+                    }
+                }
+            }
+            .theme-title{
+                padding: 20px;
+                border-bottom:1px solid rgba(239,239,239,1);
+                >h5{
+                    font-size:20px;
+                    font-family:PingFangSC-Medium;
+                    font-weight:500;
+                    color:rgba(51,51,51,1);
+                    line-height:24px;
+                }
+                >p{
+                    font-size:13px;
+                    font-family:PingFangSC-Regular;
+                    font-weight:400;
+                    color:rgba(51,51,51,1);
+                    line-height:13px;
+                    padding: 10px 0 18px;
+                }
+                >div{
+                    display: flex;
+                    align-items: center;
+                    font-size:14px;
+                    font-family:PingFangSC-Regular;
+                    font-weight:400;
+                    color:rgba(51,51,51,1);
+                    line-height:18px;
+                    >span{
+                        display: flex;
+                        width: 29px;
+                        height: 18px;
+                        align-items: center;
+                        justify-content: center;
+                        border-radius:4px;
+                        border:1px solid rgba(3,191,140,1);
+                        font-size:10px;
+                        font-family:PingFangSC-Regular;
+                        font-weight:400;
+                        color:rgba(3,191,140,1);
+                        line-height:10px;
+                        margin-right: 6px;
+                    }
+                }
+            }
+            .theme-msg{
+                padding: 20px 20px 0;
+                >div{
+                    padding-bottom: 14px;
+                    display: flex;
+                    align-items: center;
+                    justify-content: space-between;
+                    >p{
+                        font-size:14px;
+                        font-family:PingFangSC-Medium;
+                        font-weight:500;
+                        color:rgba(51,51,51,1);
+                        line-height:13px;
+                    }
+                    >div{
+                        display: flex;
+                        span{
+                            font-size:12px;
+                            font-family:PingFangSC-Regular;
+                            font-weight:400;
+                            color:rgba(153,153,153,1);
+                            line-height:12px;
+                            &:first-child{
+                                padding-right: 10px;
+                                margin-right: 10px;
+                                border-right:1px solid rgba(84,120,235,0.1);
+                            }
+                        }
+                    }
+                }
+                >p{
+                    font-size:12px;
+                    font-family:PingFangSC-Regular;
+                    font-weight:400;
+                    color:rgba(102,102,102,1);
+                    line-height:24px;
+                    overflow: hidden;
+                    text-overflow: ellipsis;
+                    display: -webkit-box;
+                    -webkit-line-clamp: 3;
+                    -webkit-box-orient: vertical;
+                    margin-bottom: 10px;
+                }
+                >dl{
+                    display: flex;
+                    justify-content: space-between;
+                    >dt{
+                        font-size:18px;
+                        font-family:PingFangSC-Medium;
+                        font-weight:500;
+                        color:rgba(255,147,0,1);
+                        line-height:20px;
+                        margin-top: 12px;
+                    }
+                    >dd{
+                         >img{
+                            display: block;
+                        }  
+                    }
+                }
+            }
+        }
+    }
 }
 .warp-right{
     width: @wapRight;
@@ -503,7 +771,7 @@ export default {
             }
         }
         >dd{
-           
+        
         }
     }
     ul.manager-list{
