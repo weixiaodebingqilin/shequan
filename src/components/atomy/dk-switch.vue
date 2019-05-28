55adb007574fee6e83427c524a1a4f5d62ca3a9c
胡轩华 <1547585832@qq.com>
2019-05-28 18:34:13 +0800

    switch

--- src/components/atomy/dk-switch.vue
+++ src/components/atomy/dk-switch.vue
@@ -1,36 +1,73 @@
 <template>
-    <div class="dk-switch">
-        <div class="dk-switch-icon"
-             @click="iconClick">
+    <div class="dk-switch"
+         @click="iconClick">
+        <div class="dk-switch-icon">
             <slot name="icon">
-                <em></em>
+                <em :class="{active: beIcon}"></em>
             </slot>
         </div>
+        <p class="dk-switch-text">
+            <slot name="text"></slot>
+        </p>
 
-        <slot name="text">
-
-        </slot>
     </div>
 </template>
 <script>
 export default {
     name: "dk-switch",
-    props: {},
+    props: {
+        likeIcon: {
+            type: Boolean,
+            default: false
+        }
+    },
     data() {
-        return {};
+        return {
+            beIcon: this.likeIcon
+        };
     },
     methods: {
-        iconClick() {}
+        iconClick() {
+            this.beIcon = !this.beIcon;
+            this.$emit("click", this.beIcon);
+        }
     }
 };
 </script>
 <style lang="less">
+@bgclor: rgba(84, 120, 235, 1);
 .dk-switch {
-    width: 100%;
     height: 20px;
     display: flex;
+    cursor: pointer;
     .dk-switch-icon {
         margin-right: 5px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        > em {
+            width: 14px;
+            height: 14px;
+            border-radius: 50%;
+            position: relative;
+            border: 1px solid @bgclor;
+            &::after {
+                content: "";
+                width: 0;
+                height: 0;
+                border-radius: 50%;
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%;-50%);
+                background: @bgclor;
+            }
+            &.active::after {
+                transition: all 0.2s ease-out;
+                width: 10px;
+                height: 10px;
+            }
+        }
     }
 }
 </style>
--- src/components/atomy/mixins.js
+++ src/components/atomy/mixins.js
@@ -5,6 +5,8 @@ const atomy = {
     tableNative: () => import('./table-native'), // 切换导航栏
     spitText: () => import('./spit-text'), // 评论内容
     showStar: () => import('./show-star'), // 星星跟评论显示
+    dkSwitch: () => import('./dk-switch'), // switch toggle 开关， 点赞跟收藏的底层组件
+
 }
 
 export default atomy
