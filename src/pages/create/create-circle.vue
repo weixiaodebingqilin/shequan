10d6897f5efe8626626f3d96cb520e9f82bddd97
李志康 <9995914197@qq.com>
2019-06-11 18:35:17 +0800

    lzk css  /create-circle

--- src/pages/create/create-circle.vue
+++ src/pages/create/create-circle.vue
@@ -3,32 +3,35 @@
         <h3>创建主题圈</h3>
         <form>
             <!-- 主题圈名 -->
-            <!-- <div class="crtcircle-name">
+            <div class="crtcircle-name">
                 <input type="text"
                        placeholder="请输入主题圈名（每月仅能修改两次）" />
-            </div> -->
+            </div>
             <!-- 上传封面 -->
-            <!-- <div class="crtcircle-cover">
-                <div></div>
+            <div class="crtcircle-cover">
+                <div>
+                    <img src="@/assets/images/temp/add-imgs.png">
+                </div>
                 <dl>
                     <dt>上传封面，它将会在圈子主页、圈子封面展示</dt>
                     <dd>建议尺寸 2560x616，支持3MB内的PNG、JPG</dd>
                 </dl>
-            </div> -->
+            </div>
             <!-- 主题圈介绍 -->
-            <!-- <div class="crtcircle-introduce">
-                <input type="text"
-                       placeholder="主题圈介绍..." />
+            <div class="crtcircle-introduce">
+                <textarea placeholder="主题圈介绍..."
+                          maxlength="200"></textarea>
                 <p>200</p>
-            </div> -->
+            </div>
             <!-- 加入费用 -->
-            <!-- <div class="crtcircle-cost">
+            <div class="crtcircle-cost">
                 加入费用：￥<input type="text"
                        placeholder="50-2000" /> /年
-            </div> -->
+            </div>
             <!-- 有效期 -->
             <div class="crtcircle-term">
                 <dk-switch-group :value="termValues"
+                                 class="term-check"
                                  @onChange='onChange'>
                     <dk-switch v-for="(item,i) in termText"
                                :key="item.text"
@@ -47,19 +50,25 @@
                 </section>
             </div>
             <!-- 规则 -->
-            <!-- <div class="crtcircle-rule">
+            <div class="crtcircle-rule">
                 <dk-switch @click="crtRule">
                     <template slot="text">
                         我已阅读并同意<span>《设圈主题圈规则》</span>
                     </template>
                 </dk-switch>
-            </div> -->
+            </div>
             <!-- 提交 -->
-            <ul>
+            <ul class="crtcircle-submit">
                 <li>取消</li>
-                <li @click="createNow">立即创建</li>
+                <li @click="createNow"
+                    class="submit-t">立即创建</li>
             </ul>
         </form>
+
+        <ul class="create-succeed">
+            <li>去上传文章 ></li>
+            <li>去主题圈（UI设计成长之家） ></li>
+        </ul>
     </div>
 </template>
 <script>
@@ -132,6 +141,216 @@ export default {
 </script>
 <style lang="less">
 .create-circle {
+    width: @wapEditor;
+    margin: 20px auto 0;
+    background: rgba(255, 255, 255, 1);
+    border-radius: 2px;
+    border: 1px solid rgba(84, 120, 235, 0.1);
+    > h3 {
+        font-size: 18px;
+        font-weight: 400;
+        color: rgba(51, 51, 51, 1);
+        line-height: 18px;
+        border-bottom: 1px solid rgba(239, 239, 239, 1);
+        padding: 20px 30px;
+    }
+    > form {
+        padding: 30px;
+    }
+    .crtcircle-name {
+        > input {
+            font-size: 20px;
+            font-weight: 500;
+            color: rgba(51, 51, 51, 1);
+            line-height: 20px;
+            border: none;
+            background: transparent;
+            width: 100%;
+            &::-webkit-input-placeholder {
+                font-size: 20px;
+                font-weight: 500;
+                color: rgba(102, 102, 102, 1);
+                line-height: 20px;
+            }
+        }
+        padding-bottom: 30px;
+        border-bottom: 1px solid rgba(239, 239, 239, 1);
+    }
+    .crtcircle-cover {
+        margin: 30px 0;
+        display: flex;
+        padding: 68px 70px;
+        border-radius: 2px;
+        border: 1px dashed rgba(84, 120, 235, 0.5);
+        > div {
+            border-right: 1px solid rgba(84, 120, 235, 0.1);
+            padding: 3px 70px 13px 0;
+            > img {
+                border: 1px dashed rgba(0, 0, 0, 0.5);
+                display: block;
+                width: 60px;
+                height: 60px;
+            }
+        }
+        > dl {
+            padding-left: 70px;
+            > dt {
+                font-size: 16px;
+                font-weight: bold;
+                color: rgba(102, 102, 102, 1);
+                line-height: 16px;
+                padding: 3px 0 20px;
+            }
+            > dd {
+                font-size: 14px;
+                font-weight: 400;
+                color: rgba(153, 153, 153, 1);
+                line-height: 24px;
+            }
+        }
+    }
+    .crtcircle-introduce {
+        position: relative;
+        padding: 20px;
+        background: rgba(247, 247, 247, 1);
+        border-radius: 2px;
+        border: 1px solid rgba(221, 221, 221, 1);
+        margin-bottom: 40px;
+        > textarea {
+            min-height: 63px;
+            width: 100%;
+            font-size: 14px;
+            font-weight: 400;
+            color: rgba(51, 51, 51, 1);
+            line-height: 14px;
+            border: none;
+            background: transparent;
+            outline: none;
+            resize: none;
+        }
+        > p {
+            position: absolute;
+            right: 20px;
+            bottom: 20px;
+            font-size: 14px;
+            font-weight: 400;
+            color: rgba(207, 207, 207, 1);
+            line-height: 14px;
+        }
+    }
+    .crtcircle-cost {
+        display: flex;
+        align-items: center;
+        font-size: 14px;
+        font-weight: 400;
+        color: rgba(51, 51, 51, 1);
+        line-height: 14px;
+        padding-bottom: 10px;
+        border-bottom: 1px solid rgba(239, 239, 239, 1);
+        > input {
+            margin: 0 20px 0 15px;
+            width: 101px;
+            height: 44px;
+            background: rgba(247, 247, 247, 1);
+            border-radius: 2px;
+            border: 1px solid rgba(221, 221, 221, 1);
+            text-indent: 20px;
+        }
+    }
+    .crtcircle-term {
+        .dk-switch-group {
+            display: flex;
+            padding: 30px 0;
+            .dk-switch {
+                margin-right: 30px;
+            }
+        }
+        > section {
+            padding: 20px 30px 15px;
+            background: rgba(247, 247, 247, 1);
+            > dl {
+                margin-bottom: 10px;
+                display: flex;
+                align-items: center;
+                > dt {
+                    font-size: 14px;
+                    font-weight: 400;
+                    color: rgba(102, 102, 102, 1);
+                    line-height: 14px;
+                }
+                > dd {
+                    font-size: 14px;
+                    font-family: PingFangSC-Regular;
+                    font-weight: 400;
+                    color: rgba(255, 147, 0, 1);
+                    line-height: 14px;
+                }
+            }
+            > p {
+                font-size: 12px;
+                font-weight: 400;
+                color: rgba(153, 153, 153, 1);
+                line-height: 24px;
+            }
+        }
+    }
+    .crtcircle-rule {
+        margin-top: 30px;
+        .dk-switch {
+            display: flex;
+            align-items: center;
+            > p {
+                font-size: 14px;
+                font-weight: 400;
+                color: rgba(51, 51, 51, 1);
+                line-height: 14px;
+            }
+            > span {
+                color: #5478eb;
+            }
+        }
+    }
+    .crtcircle-submit {
+        margin-top: 50px;
+        padding-bottom: 30px;
+        display: flex;
+        justify-content: center;
+        > li {
+            margin-left: 30px;
+            &:first-child {
+                margin-left: 0;
+            }
+            cursor: pointer;
+            width: 170px;
+            height: 36px;
+            background: rgba(102, 102, 102, 1);
+            border-radius: 2px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            font-size: 14px;
+            font-family: PingFangSC-Medium;
+            font-weight: 500;
+            color: rgba(255, 255, 255, 1);
+            line-height: 14px;
+        }
+        .submit-t {
+            background: rgba(84, 120, 235, 1);
+        }
+    }
+    .create-succeed {
+        padding: 0 30px;
+        > li {
+            font-size: 16px;
+            font-family: PingFangSC-Medium;
+            font-weight: 500;
+            color: rgba(51, 51, 51, 1);
+            line-height: 20px;
+            padding: 30px 0;
+            border-bottom: 1px solid rgba(239, 239, 239, 1);
+            cursor: pointer;
+        }
+    }
 }
 </style>
 
