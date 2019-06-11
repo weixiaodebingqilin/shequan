37351d72efcd4c5868d8e705586f545a19d5c660
lzk <995914197@qq.com>
2019-06-12 00:24:42 +0800

    lzk mine

---
+++ src/pages/mine/component/exercise.vue
@@ -0,0 +1,121 @@
+<template>
+    <ul class="exercise">
+        <li class="exercise-item" v-for="(item,index) in 7"
+                                      :key="index">
+            <div>
+                <img src="@/assets/images/temp/author-works2.png">
+                <span>1/3</span>
+            </div>
+            <h5><span><font>精</font></span>超级插画风衣服篇特辑</h5>
+            <ol>
+                <li>
+                    <svg class="icon"
+                            aria-hidden="true">
+                        <use xlink:href="#icondianzan"></use>
+                    </svg>
+                    <span>2019</span>
+                </li>
+                <li>
+                    <svg class="icon"
+                            aria-hidden="true">
+                        <use xlink:href="#icondianzan"></use>
+                    </svg>
+                    <span>2019</span>
+                </li>
+                <li>
+                    <svg class="icon"
+                            aria-hidden="true">
+                        <use xlink:href="#icondianzan"></use>
+                    </svg>
+                    <span>2019</span>
+                </li>
+            </ol>
+        </li>
+    </ul>
+</template>
+<script>
+export default {
+    name: "exercise",
+    props: {
+    }
+};
+</script>
+<style lang="less">
+/*combat-item**/
+.exercise {
+    .flex-center {
+        display: flex;
+        align-items: center;
+    }
+    display: flex;
+    flex-wrap: wrap;
+    padding-top: 30px;
+    .exercise-item{
+        width: 200px;
+        margin-right: 20px;
+        margin-bottom: 30px;
+        &:nth-child(5n+5){
+            margin-right: 0;
+        }
+        >div{
+            position: relative;
+            >img{
+                display: block;
+                width: 100%;
+                height: 200px;
+            }
+            >span{
+                position: absolute;
+                font-size:14px;
+                font-weight:500;
+                color:#666666;
+                line-height:14px;
+                right: 10px;
+                bottom: 10px;
+            }
+        }
+        >h5{
+            font-size:14px;
+            font-family:PingFangSC-Medium;
+            font-weight:bold;
+            color:rgba(51,51,51,1);
+            line-height:14px;
+            margin: 10px 0;
+            .flex-center();
+            >span{
+                .flex-center();
+                margin-right: 10px;
+                justify-content: center;
+                width:19px;
+                height:18px;
+                background:rgba(255,147,0,1);
+                border-radius:2px;
+                
+                >font{
+                    transform: scale(.8);
+                    font-size:12px;
+                    font-weight:initial;
+                    color:rgba(255,255,255,1);
+                }
+            }
+            
+        }
+        >ol{
+            .flex-center();
+            >li{
+                .flex-center();
+                flex: 1;
+                >span{
+                    font-size:12px;
+                    font-weight:400;
+                    color:rgba(207,207,207,1);
+                    line-height:14px;
+                    margin-left: 10px;
+                }
+            }
+        }
+    }
+}
+</style>
+
+
--- src/pages/mine/mymine.vue
+++ src/pages/mine/mymine.vue
@@ -2,45 +2,83 @@
     <!-- 批改作业 -->
     <div id="mymine">
         <section class="mymine-topMsg">
-            <div class="topMsg-wrap">
-                <dl @click="navToInformation">
-                    <dt><img src="@/assets/images/temp/header.png"></dt>
-                    <dd>
-                        <h5>微笑的冰欺凌</h5>
-                        <p>唐朝资深设计师，曾主导过喵付宝，天狗等项目，商务合作请联系邮箱247870295@qq.com，或私信我的助理@绚丽的小云</p>
-                    </dd>
-                </dl>
-                <ul>
-                    <li>
-                        <span>32</span>
-                        <p>篇文章看过</p>
-                    </li>
-                    <li>
-                        <span>32</span>
-                        <p>篇文章看过</p>
-                    </li>
-                    <li>
-                        <span>32</span>
-                        <p>篇文章看过</p>
-                    </li>
-                    <li>
-                        <span>32</span>
-                        <p>篇文章看过</p>
-                    </li>
-                    <li>
-                        <span>32</span>
-                        <p>篇文章看过</p>
-                    </li>
-                </ul>
+            <div class="mymine-topMsg-header"> 
+                <div class="topMsg-wrap">
+                    <dl @click="navToInformation">
+                        <dt><img src="@/assets/images/temp/header.png"></dt>
+                        <dd>
+                            <h5>微笑的冰欺凌</h5>
+                            <p>唐朝资深设计师，曾主导过喵付宝，天狗等项目，商务合作请联系邮箱247870295@qq.com，或私信我的助理@绚丽的小云</p>
+                        </dd>
+                    </dl>
+                    <ul>
+                        <li>
+                            <span>32</span>
+                            <p>篇文章看过</p>
+                        </li>
+                        <li>
+                            <span>32</span>
+                            <p>篇文章看过</p>
+                        </li>
+                        <li>
+                            <span>32</span>
+                            <p>篇文章看过</p>
+                        </li>
+                        <li>
+                            <span>32</span>
+                            <p>篇文章看过</p>
+                        </li>
+                        <li>
+                            <span>32</span>
+                            <p>篇文章看过</p>
+                        </li>
+                    </ul>
+                </div>
+                <div class="topMsg-r">
+                    <div class="lately-see-wrap">
+                        <div class="lately-see-title">
+                            <svg class="icon"
+                                aria-hidden="true">
+                                <use xlink:href="#icontuijian"></use>
+                            </svg>
+                            <span>最近看过</span>
+                        </div>
+                        <ul class="lately-see-item">
+                            <li>
+                                <p>如何画一个精致的图标？</p>
+                                <span>10分钟前</span>
+                            </li>
+                            <li>
+                                <p>网也设计中的10大坑－避险</p>
+                                <span>昨天</span>
+                            </li>
+                        </ul>
+                        <ul class="my-tag">
+                            <li>商业插画之40天训练</li>
+                            <li>ICON图标</li>
+                            <li>职场经验</li>
+                        </ul>
+                        <!-- <div class="lately-see-footer">
+                            <p>加油继续坚持，将会收获成果</p>
+                        </div> -->
+                    </div>
+                </div>
             </div>
+            
         </section>
-        <section class="main-wrap loadUp">
-            <div class="main-left">
-                <table-native :clounms="kindNative"
+        <div class="line-white"></div>
+        <table-native :clounms="kindNative" class="main-nav"
                               @tableMoveIndex="kindMove"
                               :initIndex='kindIndex'></table-native>
-
-                <!-- //文章推送 -->
+        <section class="main-wrap loadUp">
+            <div class="main-left " v-if="kindIndex!==0">
+                <!-- 我加入的 -->
+                <div class="article-push loadUp"
+                     v-if="kindIndex==1">
+                    <join-item v-for="(item,index) in 5"
+                                     :key="index" :showImg="false"></join-item>
+                </div>
+                <!-- //收藏 -->
                 <div class="article-push loadUp"
                      v-if="kindIndex==2">
                     <ul class="kind-box">
@@ -48,38 +86,17 @@
                                       :key="index"></card-artical>
                     </ul>
                 </div>
+                
 
             </div>
-
-            <div class="main-right">
-                <div class="lately-see-wrap">
-                    <div class="lately-see-title">
-                        <svg class="icon"
-                             aria-hidden="true">
-                            <use xlink:href="#icontuijian"></use>
-                        </svg>
-                        <span>最近看过</span>
-                    </div>
-                    <ul class="lately-see-item">
-                        <li>
-                            <p>如何画一个精致的图标？</p>
-                            <span>10分钟前</span>
-                        </li>
-                        <li>
-                            <p>网也设计中的10大坑－避险</p>
-                            <span>昨天</span>
-                        </li>
-                    </ul>
-                    <ul class="my-tag">
-                        <li>商业插画之40天训练</li>
-                        <li>ICON图标</li>
-                        <li>职场经验</li>
-                    </ul>
-                    <div class="lately-see-footer">
-                        <p>加油继续坚持，将会收获成果</p>
-                    </div>
-                </div>
+            <!-- "练习", -->
+            <div class="article-push loadUp"
+                    v-if="kindIndex==0">
+                <exercise></exercise>
             </div>
+            <!-- <div class="main-right">
+                
+            </div> -->
         </section>
     </div>
 </template>
@@ -87,22 +104,26 @@
 import atomy from "@/components/atomy/mixins.js";
 import molecule from "@/components/molecule/mixins.js";
 import cardArtical from "@/components/molecule/card-artical.vue";
+import exercise from "./component/exercise"; 
+import joinItem from "./component/join-item"; //我加入的--主题圈
 export default {
     name: "mymine",
     components: {
         cardArtical,
         tableNative: atomy.tableNative,
-        authorWrap: molecule.authorWrap
+        authorWrap: molecule.authorWrap,
+        exercise,
+        joinItem
     },
     data() {
         return {
             kindNative: [
-                "我的圈子",
-                "实战训练",
+                "练习",
+                "我加入的",
                 "收藏",
-                "视频",
-                "我的评论",
-                "我的评论2"
+                "评论",
+                "文章",
+                "视频"
             ], ///
             kindIndex: 2 //
         };
